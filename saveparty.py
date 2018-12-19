7d759692515882b0b892bfaa76981f7c7c4bebfb
debzou <david.petit@etu.univ-smb.fr>
2018-12-19 11:20:02 +0100

    saveparty is finish (change name in savegame)

--- saveparty.py
+++ saveparty.py
@@ -14,14 +14,33 @@ class SaveGame:
          self.game=chess.pgn.Game() #PGN format
          self.board=board
          
-    def save_game(self):
+    def save_game(self,listCoup=[]):
         """
         register a game with pgn format in txt fill
         """
+        
+        #for i in range(len(listCoup)):
+            #node = self.game.add_variation(chess.Move.from_uci("e2e4"))    
+        self.game.add_line(listCoup)            
         game_pgn = open("gamesave.txt","w",encoding="utf-8")
         register = chess.pgn.FileExporter(game_pgn)
         self.game.accept(register)
+        print(self.game)
         game_pgn.close()
+        
+    def headers(self,values=[None,None,None,None,None,None,None]):
+        """
+        allow to change head
+        """
+        labels = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
+        
+        for i in range(len(labels)):
+            
+            if values[i] !=  None :
+                print(self.game.headers[labels[i]])
+                self.game.headers[labels[i]]=values[i]
+                
+                
         
 # ===================
 # Tests
@@ -33,9 +52,11 @@ if __name__ == "__main__":
     a="a2a4"
     b=chess.Move.from_uci(a)
     board.push(b)
+    
     gam = SaveGame(board)
+    gam.headers(["test",None,None,None,None,None,None])
     print("Affichage PGN :")
-    print(gam.game)
-    gam.save_game()
+    listCoup=[chess.Move.from_uci("e2e4"),chess.Move.from_uci("b2b3"),chess.Move.from_uci("b3b4")]
+    gam.save_game(listCoup)
 
         
